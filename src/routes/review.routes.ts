29783f6fa17673009c2d426948560ece7a717ce3
Murad Alizada <alizademurad@hotmail.com>
2025-11-17 21:35:49 +0400

    Create review routes according to Task 2

--- src/routes/review.routes.ts
+++ src/routes/review.routes.ts
@@ -2,10 +2,6 @@ import { Router } from 'express';
 import { reviewController } from '../controllers/review.controller';
 
 const router = Router();
-
-/**
- * Review Routes
- */
 router.post('/movies/:movieId/reviews', reviewController.create);
 router.get('/movies/:movieId/reviews', reviewController.getByMovieId);
 router.get('/reviews/:id', reviewController.getById);
